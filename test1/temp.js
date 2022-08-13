9c70e075be845cd36cd8b7e4e6f2fd342e8146ef
FZQA <913229100@qq.com>
2022-08-13 20:07:38 +0800

    helloword

---
+++ test1/temp.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         helloworld
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  new script
+// @author       fzqa
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    console.log("hello")
+})();
